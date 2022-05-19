1180058755455540c3773803f71fe8639a7ac89f
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-05-19 12:01:40 -0500

    commit tree fe29decd34477c9abcccb2cb11286a20855250c5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {name: "Maggie", streetAdress: "1 Focus Street"};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = {...employee}
+  newEmployee[key] = value;
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = {...employee};
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+
+}
